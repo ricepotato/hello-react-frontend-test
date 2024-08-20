3f5d3705efcdfaa032cb1376f2ff26476be2811a
ricepotato <ricepotato40@gmail.com>
2024-08-20 13:16:18 +0900

    test: test 추가

--- src/components/TestForm.test.tsx
+++ src/components/TestForm.test.tsx
@@ -7,3 +7,13 @@ test("이름을 표시한다", () => {
   render(<TestForm name={name} />);
   expect(screen.getByText(name)).toBeInTheDocument();
 });
+
+test("버튼을 표시한다", () => {
+  render(<TestForm name={"ricepotato"} />);
+  expect(screen.getByRole("button")).toBeInTheDocument();
+});
+
+test("heading 을 표시한다", () => {
+  render(<TestForm name={"ricepotato"} />);
+  expect(screen.getByRole("heading")).toHaveTextContent("계정 정보");
+});
